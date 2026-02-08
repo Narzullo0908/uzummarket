f15011616cd4b81d1949dbe402c0ffdce8ea9821
makhmudov <aceraspire1771@gmail.com>
2026-02-08 17:44:11 +0500

    fixed

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -22,8 +22,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="px-25 flex justify-between items-center">
-      <div className="w-[40%] flex flex-col items-start">
+    <div className="px-25 flex justify-between items-center max-md:flex-col max-md:px-2">
+      <div className="w-[40%] flex flex-col items-start max-md:w-full">
         <p className="text-5xl uppercase">{data.title}</p>
         <div className="flex mb-20 items-center justify-start">
           {arr.map((item) => {
@@ -37,10 +37,11 @@ export default function Home() {
         </div>
         <img src={data.image} alt="" className="w-full" />
       </div>
-      <div className="w-[50%] border border-[#7000FF] rounded-[20px] p-10">
+      <div className="w-[50%] border border-[#7000FF] rounded-[20px] p-10 max-md:w-full max-md:mt-10">
         <p className="font-bold text-black/30">{data.category}</p>
         <p className="text-2xl mb-10">{data.description}</p>
-        <p className="text-3xl font-black text-[#7000FF]">${data.price}</p>
+        <p className="text-3xl font-black text-[#7000FF] mb-5">${data.price}</p>
+        <button className="w-full bg-[#7000FF] p-5 rounded-4xl text-white font-medium text-2xl">Add Card</button>
       </div>
     </div>
   );
